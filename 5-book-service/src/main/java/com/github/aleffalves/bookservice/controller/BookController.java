c66b46a930e8a1666f35a5e5259ed3323bd3a8e2
aleffalves <aleff.alves44@gmail.com>
2021-09-28 21:14:36 -0300

    "Consumindo o Endpoint de Cambio Service no Book Service"

--- 5-book-service/src/main/java/com/github/aleffalves/bookservice/controller/BookController.java
+++ 5-book-service/src/main/java/com/github/aleffalves/bookservice/controller/BookController.java
@@ -2,14 +2,17 @@ package com.github.aleffalves.bookservice.controller;
 
 import com.github.aleffalves.bookservice.model.Book;
 import com.github.aleffalves.bookservice.repository.BookRepository;
+import com.github.aleffalves.bookservice.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Date;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -26,8 +29,20 @@ public class BookController {
         Book book = bookRepository.getById(id);
         if (book == null) throw new RuntimeException("Book not Found");
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+
+        ResponseEntity<Cambio> response = new RestTemplate().getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+                Cambio.class,
+                params);
+        Cambio cambio = response.getBody();
+
         String property = environment.getProperty("local.server.port");
         book.setEnvironment(property);
+        book.setPrice(cambio.getConvertedValue());
 
         return book;
     }
