b2785289f9ec0a8b2172ec9e8cc72debe528387d
aleffalves <aleff.alves44@gmail.com>
2021-10-25 21:53:13 -0300

    "Resilience4j"

--- 6-book-service/src/main/java/com/github/aleffalves/bookservice/controller/FooBarController.java
+++ 6-book-service/src/main/java/com/github/aleffalves/bookservice/controller/FooBarController.java
@@ -1,5 +1,8 @@
 package com.github.aleffalves.bookservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +18,19 @@ public class FooBarController {
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-    @Retry(name = "foo-bar")
+//    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+//    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+//    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String fooBar(){
         logger.info("Request to foo-bar is received!");
-        var response = new RestTemplate().
-                getForEntity("http://localhost:8080/foo-bar",String.class);
-//        return "Foo-Bar!!!";
-        return response.getBody();
+//        var response = new RestTemplate().
+//                getForEntity("http://localhost:8080/foo-bar",String.class);
+        return "Foo-Bar!!!";
+//        return response.getBody();
+    }
+
+    public String fallbackMethod(Exception exception){
+        return "fallbackMethod foo-bar!!!";
     }
 }
